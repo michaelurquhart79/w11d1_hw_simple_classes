a7ce6ec96ba320b7e5e197bf81ea29b8ebb467ca
Michael Urquhart <michaelurquhart@hotmail.com>
2019-07-22 16:53:16 +0100

    tasks instructions remove from bottom of water bottle test file.

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -47,10 +47,3 @@ public class WaterBottleTest {
 
 }
 
-
-//    Water Bottle
-//    Create a water bottle class with a volume property.
-//        The volume should start at 100.
-//        Add a drink function that takes 10 from the volume each time it is called.
-//        Create an empty function that brings the volume down to 0.
-//        Create a fill function that fills the volume back to 100.
